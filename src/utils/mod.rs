5c835e3a66bf382a55bf89afc56561c93cd3d1fc
Chance Linz <clinz.dev@gmail.com>
2023-01-29 14:40:59 -0800

    now parsing html elements out of answers, prompts, categories

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,8 +1,15 @@
 pub mod sanitizer {
+    use regex::Regex;
+
     pub enum StringReplacement<'a> {
         EncodedValue { to: &'a str, from: &'a str },
     }
 
+    pub enum PatternReplacement<'a> {
+        RegexValue { pattern: Regex, to: &'a str },
+    }
+
+    /// Removes invalid characters & sequences from a string
     pub fn sanitize(s: &str) -> String {
         let mut sanitized_string = s.to_owned();
 
@@ -17,6 +24,29 @@ pub mod sanitizer {
                 StringReplacement::EncodedValue { to, from } => sanitized_string.replace(from, to),
             }
         }
+
+        // remove invalid patterns
+        let invalid_patterns = vec![
+            PatternReplacement::RegexValue {
+                pattern: Regex::new(r"</.+>").unwrap(), // closing tags
+                to: " ",
+            },
+            PatternReplacement::RegexValue {
+                pattern: Regex::new(r"<.+>").unwrap(),
+                to: " ",
+            },
+        ];
+
+        for invalid_pattern in invalid_patterns {
+            sanitized_string = match invalid_pattern {
+                PatternReplacement::RegexValue { to, pattern } => {
+                    pattern.replace(&sanitized_string, to).to_string()
+                }
+            }
+        }
+
+        // Trim start, end of string
+        sanitized_string = sanitized_string.trim().to_string();
 
         sanitized_string
     }
@@ -36,6 +66,14 @@ pub mod tests {
 
             let expected =
                 "He won the pentathlon & the decathlon at the summer Olympics in Stockholm";
+
+            assert_eq!(result, expected);
+        }
+
+        #[test]
+        fn removes_html_elements() {
+            let result = sanitize("100 YEARS AGO<span class=\"nobreak\">--</span>1912");
+            let expected = "100 YEARS AGO -- 1912";
 
             assert_eq!(result, expected);
         }
